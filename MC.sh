0bef6da1bac363827dedaaee9603f7425474eb21
Kyle Christensen <christensen.kyle@gmail.com>
2013-03-17 14:42:57 -0400

    Added new mods

--- MC.sh
+++ MC.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-nick=""
+nick="kyle"
+
+IFS="
+"
+
+# Mod list; comment out the mods to not disable with the -d flag
+BL=(
+CoFHCore
+ThermalExpansion
+buildcraft
+forestry
+industrialcraft
+)
 
 USAGE() {
    echo "
-$0 [-b]
+$0 [-d]
 
--b    Enable buildcraft
-      and forestry mod
+-d    disable blacklisted mods
+
+Blacklist currently includes:
+${BL[@]}
 "
+exit 1
 }
 
-while getopts "b" opt; do
+
+while getopts "d" opt; do
    case $opt in
-      b) BUILDCRAFT=1
+      d) DISABLE=1
          ;;
       ?)
          USAGE
@@ -23,30 +39,35 @@ done
 
 cd ~/Library/Application\ Support/minecraft/mods
 
-if [ -f build* ]; then
-   bc=`ls buildcraft*`
-   if [[ `echo $bc |grep -c jar$` != 1 ]]; then
-      if [[ $BUILDCRAFT ]]; then
-         mv $bc `echo $bc|sed -e 's/\(.*\.jar\).*/\1/'`
-      fi
-   else
-      if [[ ! $BUILDCRAFT ]]; then
-         mv $bc ${bc}.disabled
+for i in ${BL[@]}; do
+   if [ -f $i* ]; then
+      fn=`ls $i*`
+      if [[ `echo $fn |grep -c "jar$"` != 1 ]] && [[ `echo $fn |grep -c "zip$"` != 1 ]]; then
+         # .jar or .zip
+         if [[ ! $DISABLE ]]; then
+            mv $fn `echo $fn|sed -e 's/\(.*\)\.disabled/\1/'`
+            if [[ $? != 0 ]]; then
+               ERR=1
+               echo ERROR: $fn
+               echo mv $fn `echo $fn|sed -e 's/\(.*\)\.disabled/\1/'`
+            fi
+         fi
+      else
+         echo else
+         if [[ $DISABLE ]]; then
+            mv $fn ${fn}.disabled
+            if [[ $? != 0 ]]; then
+               ERR=1
+               echo ERROR: $i
+               echo mv $fn ${fn}.disabled
+            fi
+         fi
       fi
    fi
-fi
+done
 
-if [ -f forestry* ]; then
-   fm=`ls forestry*`
-   if [[ `echo $fm |grep -c jar$` != 1 ]]; then
-      if [[ $BUILDCRAFT ]]; then
-         mv $fm `echo $fm|sed -e 's/\(.*\.jar\).*/\1/'`
-      fi
-   else
-      if [[ ! $BUILDCRAFT ]]; then
-         mv $fm ${fm}.disabled
-      fi
-   fi
+if [[ $ERR == 1 ]]; then
+   exit 1
 fi
 
 cd ..
